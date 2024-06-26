d9828b577195d0bfa3b0eb0ab16a4515502b0205
Kenneth Morales <141078662+ElectricPorpoise452@users.noreply.github.com>
2024-06-26 07:06:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+/* Script For All Pages */
 
+function toggle() {
+var x =
+document.getElementById(fun-menu);
+  if(x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none"
+  }
+}
+
+/* Script For Home Page */
+
+/* Script For About Page */
+
+/* Script For Contact Page */
